9365b1944722fb8e5db0907f1c0b940079472880
WaiEnu <strayhituji@gmail.com>
2021-05-05 13:08:13 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WaiEnu/work-space-craft-dot-com/precache-manifest.003ffba6dc23cc92e6356e43322ffe59.js"
+  "./precache-manifest.7919878ffab3bd4cfcbcfd6c6f277d88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WaiEnu/work-space-craft-dot-com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
